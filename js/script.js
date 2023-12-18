62232ef003d589898037e85fb82b584969acf4e6
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2023-12-18 10:12:46 +0700

    Memperbaiki javascript menjadi lebih rapi

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ function getAll(){
     var name=document.getElementById("name").value; 
     var birthdate=document.getElementById("birthdate").value; 
     var gender=document.querySelector('input[name="gender"]:checked').value;
-    var message=document.getElementById("message").value; 
+    var message=document.getElementById("message").value;
+    var time = "<strong >Current Time : </strong> ";
+    name = "<strong>Nama : </strong>"+name;
+    birthdate="<strong >Tanggal Lahir : </strong>"+birthdate;
+    gender="<strong>Jenis Kelamin : </strong>"+gender;
+    message="<strong>Pesan : </strong>"+message;
+    document.getElementById("waktu").innerHTML = time;
     document.getElementById("nama").innerHTML = name;
     document.getElementById("lahir").innerHTML = birthdate;
     document.getElementById("kelamin").innerHTML = gender;
