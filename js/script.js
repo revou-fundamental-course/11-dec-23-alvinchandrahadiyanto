1c57dbd0069b96f26bbefc031e7bf391f1a7291f
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2023-12-18 23:36:23 +0700

    Menambahkan validasi

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-function getAll(){ 
+function getAll(){
+    if (!document.querySelector('input[name="gender"]:checked')) {
+        alert('Data tidak boleh ada yang kosong');
+        return false;
+    }
     const date = new Date();
     var name=document.getElementById("name").value; 
     var birthdate=document.getElementById("birthdate").value; 
     var gender=document.querySelector('input[name="gender"]:checked').value;
     var message=document.getElementById("message").value;
-    var time = "<strong >Current Time : </strong> "+date;
-    name = "<strong>Nama : </strong>"+name;
-    birthdate="<strong >Tanggal Lahir : </strong>"+birthdate;
-    gender="<strong>Jenis Kelamin : </strong>"+gender;
-    message="<strong>Pesan : </strong>"+message;
-    document.getElementById("waktu").innerHTML = time;
-    document.getElementById("nama").innerHTML = name;
-    document.getElementById("lahir").innerHTML = birthdate;
-    document.getElementById("kelamin").innerHTML = gender;
-    document.getElementById("pesan").innerHTML = message;
+    var time = "<strong>Current Time : </strong> "+date;
+    if(name===null || birthdate===null || message===""){
+        alert("Data tidak boleh ada yang kosong");
+        return false;
+    } else {
+        name = "<strong>Nama : </strong>"+name;
+        birthdate="<strong>Tanggal Lahir : </strong>"+birthdate;
+        gender="<strong>Jenis Kelamin : </strong>"+gender;
+        message="<strong>Pesan : </strong>"+message;
+        document.getElementById("waktu").innerHTML = time;
+        document.getElementById("nama").innerHTML = name;
+        document.getElementById("lahir").innerHTML = birthdate;
+        document.getElementById("kelamin").innerHTML = gender;
+        document.getElementById("pesan").innerHTML = message;
+        return true;
+    }
 }
 
 function resetAll(){ 
